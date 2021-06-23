6046bd49fba91f7fd93c090ba91a8d9b65ff54ea
krzem <lech.krzeminski@renderro.com>
2021-06-23 10:27:28 +0200

    hidden credentials

--- src/client_locator.py
+++ src/client_locator.py
@@ -1,11 +1,12 @@
 import boto3
+from src.aws_keys import AWS_SECRET_KEY, AWS_ACCESS_KEY
 
 class ClientLocator:
     def __init__(self, client):
         self._client = boto3.client(
             client,
-            aws_access_key_id="AKIAU4L6DO3BFJLNG4GM",
-            aws_secret_access_key= "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",
+            aws_access_key_id=AWS_ACCESS_KEY,
+            aws_secret_access_key=AWS_SECRET_KEY,
             region_name='eu-central-1'
             )
 
